18f866d84f40315309a7cf5a96de7f127ccb7d9e
hitesh <hitesh.evd@gmail.com>
2023-02-15 22:38:50 +0530

    add admin site register

--- school/school/app/admin.py
+++ school/school/app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Teacher, Student, Parent
+from .models import Teacher, Student, Parent,Class,ClassRoom,Enrollment,Subject,Attendance,Grade
 
 admin.site.register(Student)
 admin.site.register(Teacher)
-admin.site.register(Parent)
+admin.site.register(Class)
+admin.site.register(ClassRoom)
+admin.site.register(Enrollment)
+admin.site.register(Subject)
+admin.site.register(Attendance)
+admin.site.register(Grade)
+
